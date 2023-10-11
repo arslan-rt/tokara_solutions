ba04c21483d654f627ec78e684c9c9c85dc44c0a
arslan-rt <arslan.muhammad@rolustech.com>
2023-10-11 11:52:44 +0500

    After adding the language file for the Admin panel

---
+++ custom/Extension/modules/Home/Ext/Language/en_us.integration_urls.php
@@ -0,0 +1,8 @@
+<?php
+
+$mod_strings["LBL_URL"] = "Enter URL:";
+$mod_strings["LBL_CONNECTION_SUCCESS"] = "Connection is created Successfully!";
+$mod_strings["LBL_TEST_CONNECTION"] = "Save";
+$mod_strings["LBL_CONNECTION_FAILED"] = "Connection is Failed!";
+$mod_strings["LBL_AUTHERIZATION_RETERIVE_ERROR"] = "Please refresh the page as your meta is out of date.";
+$mod_strings["LBL_AUTH_KEY"] = "Enter Autherization Key:";
